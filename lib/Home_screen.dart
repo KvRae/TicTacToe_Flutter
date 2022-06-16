79f7b38e591dfab67cfae6f46e9909c4ddaf53b1
KvRae <karamelmannai@gmail.com>
2022-06-16 14:59:17 +0100

    Adding UI Elements

--- lib/Home_screen.dart
+++ lib/Home_screen.dart
@@ -13,13 +13,75 @@ class HomePage extends StatefulWidget {
 
 }
 class _HomePageState extends State<HomePage> {
+  String activePlayer = 'X';
+  bool gameOver = false;
+  int turn = 0;
+  String result = 'x player';
+
+  //Game game = Game
+  bool isSwitched = false;
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Page'),
-      ),
-    );
+      backgroundColor: Theme.of(context).primaryColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            SwitchListTile.adaptive(
+              title: const Text('2Players Mode',
+              style: TextStyle(color: Colors.white,
+              fontSize: 25
+              ),
+              textAlign: TextAlign.center,
+              ),
+
+              value: isSwitched,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isSwitched = newValue;
+                });
+              },
+            ),
+            Text("It's $activePlayer turn".toUpperCase(),
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 30
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Text(result,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 30
+              ),
+              textAlign: TextAlign.center,
+            ),
+            ElevatedButton.icon(onPressed: (){
+                  setState((){
+                     activePlayer = 'X';
+                     gameOver = false;
+                     turn = 0;
+                     result = '';
+                  }
+              );
+            },
+                icon: const Icon(Icons.replay),
+                label: const Text('Restart'),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Theme.of(context).splashColor),
+              ),
+            ),
+
+          ],
+        )
+      )
+
+     );
   }
 
 }
---
+++ lib/game_logic.dart
@@ -0,0 +1,11 @@
+class Player {
+  String name;
+  int score;
+  Player(this.name, this.score);
+}
+
+class Game {
+  Player player1;
+  Player player2;
+  Game(this.player1, this.player2);
+}
